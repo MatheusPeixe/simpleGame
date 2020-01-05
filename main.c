6db482bd63ec3eb38d2460d1179b0f270a30698f
MatheusPeixe <matheus.hpeixe@gmail.com>
2020-01-05 02:06:25 -0300

    ADD mapeamento de classe -criadas as defines para as classes disponiveis -criado os arrays para acessar os valores

--- main.c
+++ main.c
@@ -1,5 +1,25 @@
 #include<stdio.h>
 
+#define TOTAL_CLASSE 4
+#define CLASSE_ARQUEIRO 0
+#define CLASSE_GUERREIRO 1
+#define CLASSE_MAGO 2
+#define CLASSE_BARBARO 3
+
 int main(){
-    return 0;
+	int classesDisponiveis[TOTAL_CLASSE] = {
+		CLASSE_ARQUEIRO,
+		CLASSE_GUERREIRO,
+		CLASSE_MAGO,
+		CLASSE_BARBARO,
+	};
+
+	char *classesDisponiveisTexto[TOTAL_CLASSE] = {
+		"Arquiero",
+		"Guerreiro",
+		"Mago",
+		"Barbaro",
+	};
+
+  return 0;
 }
