a6032cb57a2d68080ff66ec424a2edccf5c9d529
MatheusPeixe <matheus.hpeixe@gmail.com>
2020-01-05 05:49:22 -0300

    -equipamento Individual -equipamento por classe -define de item por classe -struct dos equipamentos (usados tanto nos iniciais quanto por classe individual

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<locale.h>
 
+#define TOTAL_ITEM_CLASSE 5
 #define TOTAL_BOSSES 5
 #define TOTAL_CLASSE 4
 #define CLASSE_ARQUEIRO 1
@@ -19,6 +20,13 @@ typedef struct {
   int dano;
   int vida;
 } boss;
+
+typedef struct {
+  char nome[30];
+  int preco;
+  int danoBase;
+  int danoEspecial;
+} equipamento;
 
 int main(){
 	int classesDisponiveis[TOTAL_CLASSE] = {
@@ -41,6 +49,45 @@ int main(){
     {"Vicar Amelia", 80, 900},
     {"One Reborn", 90, 1000},
     {"Gehrman, The First Hunter", 100, 1500},
+  };
+
+  equipamento equipamentosInicial[TOTAL_CLASSE] = {
+    {"Girombinator", 0, 60, 100},
+    {"LongSword", 0, 80, 110},
+    {"Pirulinator", 0, 70, 100},
+    {"BattleAxe", 0, 90, 100},
+  };
+
+  equipamento equipamentoArqueiro[TOTAL_ITEM_CLASSE] = {
+    {"Arco Curvo", 100, 65, 100},
+    {"Arco Longo", 160, 70, 110},
+    {"Besta de 2 Flecha", 300, 130, 150},
+    {"Arco Celeste", 330, 150, 160},
+    {"Arco Sagrado", 500, 200, 200},
+  };
+
+  equipamento equipamentosGuerreiro[TOTAL_ITEM_CLASSE] = {
+    {"ClayMore", 100, 65, 100},
+    {"Tempestade Negra", 160, 70, 110},
+    {"Rapieira", 200, 80, 130},
+    {"Espada de Gelo", 300, 90, 130},
+    {"Lamina da Morte", 500, 100, 150},
+  };
+
+  equipamento equipamentosMago[TOTAL_ITEM_CLASSE] = {
+    {"Cajado da Ameaça Vaporosa", 100, 65, 100,},
+    {"Cajado Beatífico Menor", 160, 70, 100,},
+    {"Cajado Beatífico Maior", 200, 80, 110,},
+    {"Cajado do Carcereiro", 300, 90, 130},
+    {"Cajado Carvalho da batalha", 500, 100, 150},
+  };
+
+  equipamento equipamentosBarbaro[TOTAL_ITEM_CLASSE] = {
+    {"Machado Grande", 100, 60, 100},
+    {"Martelo", 160, 70, 100},
+    {"Mangual", 200, 80, 120},
+    {"Machado de 2 Mãos", 290, 90, 130},
+    {"Machado Estrela", 400, 100, 150},
   };
 
   setlocale(LC_ALL,"");
