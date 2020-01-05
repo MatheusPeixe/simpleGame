793222a8e71b82791d0079269c4cb591120e85db
bertoccoj <joaovitorbertocco51@gmail.com>
2020-01-05 03:05:15 -0300

    pede e armazena informações do jogador

--- main.c
+++ main.c
@@ -28,5 +28,35 @@ int main(){
 		"Barbaro",
 	};
 
+  setlocale(LC_ALL,"");
+
+  int i, j;
+  player jogador;
+
+  printf("Como deseja ser chamado?(obs: maximo 100 caracteres)\n -->");
+  scanf("%s", &jogador.nickName);
+
+  int classeEscolhidainvalida;
+
+  do {
+    system("clear");
+    printf("Escolha uma das classes disponiveis:\n");
+
+    for(i = 0; i < TOTAL_CLASSE; i++) {
+      printf("\n%d - %s", i + 1, classesDisponiveisTexto[i]);
+    }
+    printf("\n-->");
+
+    scanf("%d", &jogador.classe);
+    jogador.classe--;
+
+    classeEscolhidainvalida = jogador.classe < 0 || jogador.classe > 4;
+    if (classeEscolhidainvalida) {
+      system("read -p \"Opção escolhida invalida\" saindo");
+    }
+  } while(classeEscolhidainvalida);
+
+  printf("jogador: %s escolheu a classe %s", jogador.nickName, classesDisponiveisTexto[jogador.classe]);
+
   return 0;
 }
